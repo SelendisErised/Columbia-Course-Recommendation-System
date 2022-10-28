aa4917fd165fa7cc19692707a487250abe5f6e41
xinhuang09 <xh2510@columbia.edu>
2022-10-28 11:05:18 -0400

    Added sketch resourses folder

---
+++ UI Design Resource - Sketch/data/story.js
@@ -0,0 +1,108 @@
+var story = {
+ "docName": "6156-project-sketch",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "6156 Project - Sketch",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "517AD6F8-ABFD-44EA-9936-A4F8A5AA1D52",
+   "groupID": "3003A545-F172-47AB-81AA-44866A6D4006",
+   "index": 0,
+   "image": "web_home.png",
+   "image2x": "web_home@2x.png",
+   "width": 1920,
+   "height": 1080,
+   "x": 186,
+   "y": 273,
+   "title": "Web - Home",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "67AA02D9-89AC-4C88-90C7-72E0344D7C4E",
+   "groupID": "207ED64F-E473-4FD5-91FA-ACBDCE0DA9BD",
+   "index": 1,
+   "image": "web_search.png",
+   "image2x": "web_search@2x.png",
+   "width": 1920,
+   "height": 1080,
+   "x": -961,
+   "y": -512,
+   "title": "Web - Search",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "23CE7724-FBB9-4CAC-8D50-76417F155BE5",
+   "groupID": "82E52B29-E5D2-4951-8311-23376190DC83",
+   "index": 2,
+   "image": "web_about.png",
+   "image2x": "web_about@2x.png",
+   "width": 1920,
+   "height": 1080,
+   "x": -961,
+   "y": -512,
+   "title": "Web - About",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "3003A545-F172-47AB-81AA-44866A6D4006",
+   "name": "Home"
+  },
+  {
+   "id": "207ED64F-E473-4FD5-91FA-ACBDCE0DA9BD",
+   "name": "Search"
+  },
+  {
+   "id": "82E52B29-E5D2-4951-8311-23376190DC83",
+   "name": "About"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 0,
+ "totalImages": 3
+}
