a69f96629a858032ef6264ae71104d2fa63d2826
Yuxian Chen <cyx980901@163.com>
2022-12-02 11:43:21 -0500

    evaluation data ver 4

--- Data/create_evaluation.py
+++ Data/create_evaluation.py
@@ -7,27 +7,27 @@ COURSE_VALUATE_PATH = './course_evaluation.csv'
 
 
 def main():
-    df = pd.DataFrame(pd.read_csv('course.csv'))
+    df = pd.DataFrame(pd.read_csv('data_all_raw.csv'))
     df = df.drop(columns=['CourseTitle', 'CourseSubtitle', 'CallNumber',
                           'Term', 'NumFixedUnits', 'Time', 'Location', 'Tag'])
 
     df['Course'] = df['Course'].map(lambda string: string[0:8])
-    df.loc[284] = ['ELEN6882', 'WANG, XIAODONG']
-    df.loc[313] = ['COMS4705', 'BENAJIBA, YASSINE']
     df = df.drop_duplicates()
+    df.loc[1] = ['COMS4111', 'FERGUSON, DONALD F']
+    df.loc[284] = ['ELEN6882', 'WANG, XIAODONG']
+    df = df.drop_duplicates()
+    print(df.info())
 
     num = len(df['Course'])
-    df['Workload'], df['Accessibility'] = np.random.randint(1, 6, num), np.random.randint(1, 6, num)
-    df['Delivery'], df['Difficulty'] = np.random.randint(1, 6, num), np.random.randint(1, 6, num)
-    # print(df.info())
+    df['Workload'], df['Accessibility'] = 2 * np.random.random(num) + 3, 2 * np.random.random(num) + 3
+    df['Delivery'], df['Difficulty'] = 2 * np.random.random(num) + 3, 2 * np.random.random(num) + 3
+    df['Cnt'] = np.random.randint(200, 501, num)
+    print(df.info())
 
-    if not os.path.exists(COURSE_VALUATE_PATH):
-        df.to_csv(COURSE_VALUATE_PATH, index=None)
-        df_new = pd.DataFrame(pd.read_csv(COURSE_VALUATE_PATH))
-        print(df_new.info())
-    else:
-        df_new = pd.DataFrame(pd.read_csv(COURSE_VALUATE_PATH))
-        print(df_new.info())
+
+    df.to_csv(COURSE_VALUATE_PATH, index=None)
+    df_new = pd.DataFrame(pd.read_csv(COURSE_VALUATE_PATH))
+    print(df_new.info())
 
 
 if __name__ == '__main__':
