d0a8698d90578909e444b672847665b4f8ceb172
Jinxuan Tang <91619567+SelendisErised@users.noreply.github.com>
2022-12-10 11:07:55 -0500

    requirement

--- Proj/server.py
+++ Proj/server.py
@@ -8,8 +8,8 @@ from dbfunction import DatabaseConnection, SearchFunction, EvaluationFunction
 from flask_oauth import OAuth
 
 host = 'localhost'
-database_user_id = 'root'
-database_user_password = 'hx687099'
+database_user_id = 'Jinxuan_Tang'
+database_user_password = 'Yky722104$'
 default_scheme = '6156_project'
 
 def create_cursor(host = host, database_user_id = database_user_id, database_user_password = database_user_password, default_scheme = default_scheme):
