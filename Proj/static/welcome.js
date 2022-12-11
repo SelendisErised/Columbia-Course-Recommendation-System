cb0b2891d7406f9c3d69a67ec53fe14ea8771535
ChenhaoWang1999 <ChenhaoWang1999@qq.com>
2022-12-11 14:53:33 -0500

    bug fix: welcome page search function fixed

--- Proj/static/welcome.js
+++ Proj/static/welcome.js
@@ -1,13 +1,13 @@
 
 $(document).ready(function () {
 
-    // https://developers.google.com/identity/openid-connect/openid-connect#obtainuserinfo
-    let info = parseJwt(data.id_token)
-    console.log(data)
-    console.log(info)
-    $("#user_email").html("Hi! " + info.email);
-    sessionStorage.setItem('user_email', info.email)
-    $("#user_email_nav").html(sessionStorage.getItem('user_email'));
+    // // https://developers.google.com/identity/openid-connect/openid-connect#obtainuserinfo
+    // let info = parseJwt(data.id_token)
+    // console.log(data)
+    // console.log(info)
+    // $("#user_email").html("Hi! " + info.email);
+    // sessionStorage.setItem('user_email', info.email)
+    // $("#user_email_nav").html(sessionStorage.getItem('user_email'));
 
     const form  = document.getElementById('search_form_welcome');
     form.addEventListener('submit', (event) => {
