e63b10127bf5a9c873a1a252092a10cc9b30947d
Jinxuan Tang <91619567+SelendisErised@users.noreply.github.com>
2022-12-12 22:14:37 -0500

    sns_sub & requirement update

--- Proj/sns_sub.py
+++ Proj/sns_sub.py
@@ -1,4 +1,10 @@
+import json
+import logging
 import boto3
+import time
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
 
 
 class SnsWrapper:
@@ -9,13 +15,54 @@ class SnsWrapper:
         """
         self.sns_resource = sns_resource
 
-    def subscribe(self, topic, protocol, endpoint):
+    def create_topic(self, name):
+        """
+        Creates a notification topic.
+        :param name: The name of the topic to create.
+        :return: The newly created topic.
+        """
+        try:
+            topic = self.sns_resource.create_topic(Name=name)
+            logger.info("Created topic %s with ARN %s.", name, topic.arn)
+        except ClientError:
+            logger.exception("Couldn't create topic %s.", name)
+            raise
+        else:
+            return topic
+
+    def list_topics(self):
+        """
+        Lists topics for the current account.
+        :return: An iterator that yields the topics.
+        """
+        try:
+            topics_iter = self.sns_resource.topics.all()
+            logger.info("Got topics.")
+        except ClientError:
+            logger.exception("Couldn't get topics.")
+            raise
+        else:
+            return topics_iter
+
+    @staticmethod
+    def delete_topic(topic):
+        """
+        Deletes a topic. All subscriptions to the topic are also deleted.
+        """
+        try:
+            topic.delete()
+            logger.info("Deleted topic %s.", topic.arn)
+        except ClientError:
+            logger.exception("Couldn't delete topic %s.", topic.arn)
+            raise
+
+    @staticmethod
+    def subscribe(topic, protocol, endpoint):
         """
         Subscribes an endpoint to the topic. Some endpoint types, such as email,
         must be confirmed before their subscriptions are active. When a subscription
         is not confirmed, its Amazon Resource Number (ARN) is set to
         'PendingConfirmation'.
-
         :param topic: The topic to subscribe to.
         :param protocol: The protocol of the endpoint, such as 'sms' or 'email'.
         :param endpoint: The endpoint that receives messages, such as a phone number
@@ -34,12 +81,168 @@ class SnsWrapper:
         else:
             return subscription
 
-if __name__ == '__main__':
-    topic_arn = 'arn:aws:sns:us-east-1:494505086554:Course_System'
-    protocol = 'email'
-    endpoint = 'jt3302@columbia.edu'
-    client = boto3.client('sns')
+    def list_subscriptions(self, topic=None):
+        """
+        Lists subscriptions for the current account, optionally limited to a
+        specific topic.
+        :param topic: When specified, only subscriptions to this topic are returned.
+        :return: An iterator that yields the subscriptions.
+        """
+        try:
+            if topic is None:
+                subs_iter = self.sns_resource.subscriptions.all()
+            else:
+                subs_iter = topic.subscriptions.all()
+            logger.info("Got subscriptions.")
+        except ClientError:
+            logger.exception("Couldn't get subscriptions.")
+            raise
+        else:
+            return subs_iter
 
-    sns_resource = client
-    Subscription = SnsWrapper(sns_resource)
-    Subscription.subscribe(topic_arn, protocol, endpoint)
+    @staticmethod
+    def add_subscription_filter(subscription, attributes):
+        """
+        Adds a filter policy to a subscription. A filter policy is a key and a
+        list of values that are allowed. When a message is published, it must have an
+        attribute that passes the filter or it will not be sent to the subscription.
+        :param subscription: The subscription the filter policy is attached to.
+        :param attributes: A dictionary of key-value pairs that define the filter.
+        """
+        try:
+            att_policy = {key: [value] for key, value in attributes.items()}
+            subscription.set_attributes(
+                AttributeName='FilterPolicy', AttributeValue=json.dumps(att_policy))
+            logger.info("Added filter to subscription %s.", subscription.arn)
+        except ClientError:
+            logger.exception(
+                "Couldn't add filter to subscription %s.", subscription.arn)
+            raise
+
+    @staticmethod
+    def delete_subscription(subscription):
+        """
+        Unsubscribes and deletes a subscription.
+        """
+        try:
+            subscription.delete()
+            logger.info("Deleted subscription %s.", subscription.arn)
+        except ClientError:
+            logger.exception("Couldn't delete subscription %s.", subscription.arn)
+            raise
+
+    def publish_text_message(self, phone_number, message):
+        """
+        Publishes a text message directly to a phone number without need for a
+        subscription.
+        :param phone_number: The phone number that receives the message. This must be
+                             in E.164 format. For example, a United States phone
+                             number might be +12065550101.
+        :param message: The message to send.
+        :return: The ID of the message.
+        """
+        try:
+            response = self.sns_resource.meta.client.publish(
+                PhoneNumber=phone_number, Message=message)
+            message_id = response['MessageId']
+            logger.info("Published message to %s.", phone_number)
+        except ClientError:
+            logger.exception("Couldn't publish message to %s.", phone_number)
+            raise
+        else:
+            return message_id
+
+    @staticmethod
+    def publish_message(topic, message, attributes):
+        """
+        Publishes a message, with attributes, to a topic. Subscriptions can be filtered
+        based on message attributes so that a subscription receives messages only
+        when specified attributes are present.
+        :param topic: The topic to publish to.
+        :param message: The message to publish.
+        :param attributes: The key-value attributes to attach to the message. Values
+                           must be either `str` or `bytes`.
+        :return: The ID of the message.
+        """
+        try:
+            att_dict = {}
+            for key, value in attributes.items():
+                if isinstance(value, str):
+                    att_dict[key] = {'DataType': 'String', 'StringValue': value}
+                elif isinstance(value, bytes):
+                    att_dict[key] = {'DataType': 'Binary', 'BinaryValue': value}
+            response = topic.publish(Message=message, MessageAttributes=att_dict)
+            message_id = response['MessageId']
+            logger.info(
+                "Published message with attributes %s to topic %s.", attributes,
+                topic.arn)
+        except ClientError:
+            logger.exception("Couldn't publish message to topic %s.", topic.arn)
+            raise
+        else:
+            return message_id
+
+    @staticmethod
+    def publish_multi_message(
+            topic, subject, default_message, sms_message, email_message):
+        """
+        Publishes a multi-format message to a topic. A multi-format message takes
+        different forms based on the protocol of the subscriber. For example,
+        an SMS subscriber might receive a short, text-only version of the message
+        while an email subscriber could receive an HTML version of the message.
+        :param topic: The topic to publish to.
+        :param subject: The subject of the message.
+        :param default_message: The default version of the message. This version is
+                                sent to subscribers that have protocols that are not
+                                otherwise specified in the structured message.
+        :param sms_message: The version of the message sent to SMS subscribers.
+        :param email_message: The version of the message sent to email subscribers.
+        :return: The ID of the message.
+        """
+        try:
+            message = {
+                'default': default_message,
+                'sms': sms_message,
+                'email': email_message
+            }
+            response = topic.publish(
+                Message=json.dumps(message), Subject=subject, MessageStructure='json')
+            message_id = response['MessageId']
+            logger.info("Published multi-format message to topic %s.", topic.arn)
+        except ClientError:
+            logger.exception("Couldn't publish message to topic %s.", topic.arn)
+            raise
+        else:
+            return message_id
+
+
+if __name__ == '__main__':
+    protocol = 'email'
+    email = 'jt3302@columbia.edu'
+    phone_number = '+19177423181'
+
+    sns_wrapper = SnsWrapper(boto3.resource('sns'))
+
+    topic_name = f'Course_System-{time.time_ns()}'
+    print(f"Creating topic {topic_name}.")
+    topic = sns_wrapper.create_topic(topic_name)
+
+    if phone_number != '':
+        print(f"Sending an SMS message directly from SNS to {phone_number}.")
+        sns_wrapper.publish_text_message(phone_number, 'Hello from the SNS demo!')
+
+    # sns_wrapper.subscribe(topic_arn, protocol, endpoint)
+    if email != '':
+        print(f"Subscribing {email}.")
+        email_sub = sns_wrapper.subscribe(topic, 'email', email)
+        answer = input(
+            f"Confirmation email sent to {email}. To receive SNS messages, "
+            f"follow the instructions in the email. When confirmed, press "
+            f"Enter to continue.")
+        while (email_sub.attributes['PendingConfirmation'] == 'true'
+               and answer.lower() != 's'):
+            answer = input(
+                f"Email address {email} is not confirmed. Follow the "
+                f"instructions in the email to confirm and receive SNS messages. "
+                f"Press Enter when confirmed or enter 's' to skip. ")
+            email_sub.reload()
